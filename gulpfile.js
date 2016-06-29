ce16ca9ee8f1cd1482a30b4eee2677ae8723e611
Lee Igor <glazo3x@gmail.com>
2016-06-29 11:57:34 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var wait = require('gulp-wait');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+
+gulp.task('default',['sass', 'sync'], function() {
+	gulp.watch("./*.html", "sync");
+	gulp.watch("./styles/sass/**/*.scss", "sass");
+});
+
+gulp.task('sass', function() {
+	gulp.src('./styles/index.scss')
+	.pipe(sourcemaps.init())
+	.pipe(wait(200))
+	.pipe(sass())
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('./styles/'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('sync', function() {
+	browserSync({
+		server: {
+			baseDir: "./"
+		},
+		notify: false
+	});
+});
