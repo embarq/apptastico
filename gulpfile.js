d1aef2691cad81aba01f0cafd8d9f654ce2a8940
Lee Igor <glazo3x@gmail.com>
2016-06-30 09:02:48 +0300

    `advanteges` widget

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,16 @@ var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
 
 gulp.task('default',['sass', 'sync'], function() {
-	gulp.watch("./*.html", browserSync.reload);
-	gulp.watch("./styles/sass/**/*.scss", "sass");
+	gulp.watch("*.html", browserSync.reload);
+	gulp.watch("styles/**/*.scss", ["sass"]);
 });
 
 gulp.task('sass', function() {
-	gulp.src('./styles/index.scss')
-	.pipe(sourcemaps.init())
-	.pipe(wait(200))
-	.pipe(sass())
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest('./styles/'))
-	.pipe(browserSync.reload({stream: true}));
+	gulp.src('styles/index.scss')
+		.pipe(wait(200))
+		.pipe(sass())
+		.pipe(gulp.dest('styles/'))
+		.pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('sync', function() {
